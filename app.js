dfb6b1946c57b1fe9c62eef30695b36063347be6
Kinshuk Ghildial <dev.kghildial@gmail.com>
2017-11-17 18:07:54 +0530

    Add create comment route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Event = require('./models/event')
 var seedDB = require('./seeds');
+var Comment = require('./models/comment');
 
 seedDB();
 mongoose.connect('mongodb://localhost/eventdb');
@@ -42,11 +43,37 @@ app.get('/events/:id', function(req, res){
   });
 });
 
-
 //Add new comment form
 app.get('/events/:id/comments/new', function(req, res){
   Event.findById(req.params.id, function(err, event){
     res.render('comments/new', {event: event});
+  });
+});
+
+//Create comment route
+app.post('/events/:id/comments', function(req, res){
+  console.log(req.body);
+  Event.findById(req.params.id, function(err, event){
+    if(err){
+      console.log(err);
+        res.redirect('back');
+    }
+    else{
+      console.log(req.body);
+      Comment.create(req.body.comment, function(err, comment){
+        if(err){
+          console.log(err);
+        }
+        else{
+          comment.author = req.body.author;
+          comment.text = req.body.text;
+          comment.save();
+          event.comments.push(comment);
+          event.save();
+          res.redirect('/events/' + event._id);
+        }
+      });
+    }
   });
 });
 
