ef22be9d722665b53511caa26ba57a48f8f9c2f5
Kinshuk Ghildial <dev.kghildial@gmail.com>
2017-11-17 17:50:59 +0530

    Seed comments in db

--- models/event.js
+++ models/event.js
@@ -6,7 +6,13 @@ var eventSchema = new mongoose.Schema({
   organiser: String,
   location: String,
   description: String,
-  ticket_price: Number
+  ticket_price: Number,
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment'
+    }
+  ]
 });
 
 module.exports = mongoose.model('Eventpost', eventSchema);
--- seeds.js
+++ seeds.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Event = require('./models/event');
+var Comment = require('./models/comment');
 
 var data = [
   {
@@ -28,6 +29,21 @@ function seedDB(){
         else{
           console.log('Added an event!');
         }
+        Comment.create(
+          {
+            text: 'Hi!',
+            author: 'User1'
+          },
+          function(err, comment){
+          if(err){
+            console.log(err);
+          }
+          else{
+            event.comments.push(comment);
+            event.save();
+            console.log('Added comments!');
+          }
+        });
       });
     });
   });
